ebf042831579ebfdfaed620bddbf9cb52bf57ebe
neliSun <nelson.chow91.nc@gmail.com>
2017-03-14 21:28:50 -0700

    Created payment gateway class to process payments.

---
+++ PaymentsApp/PaymentGateway.h
@@ -0,0 +1,13 @@
+//
+//  PaymentGateway.h
+//  PaymentsApp
+//
+//  Created by Nelson Chow on 2017-03-14.
+//  Copyright © 2017 Nelson Chow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PaymentGateway : NSObject
+
+@end
---
+++ PaymentsApp/PaymentGateway.m
@@ -0,0 +1,13 @@
+//
+//  PaymentGateway.m
+//  PaymentsApp
+//
+//  Created by Nelson Chow on 2017-03-14.
+//  Copyright © 2017 Nelson Chow. All rights reserved.
+//
+
+#import "PaymentGateway.h"
+
+@implementation PaymentGateway
+
+@end

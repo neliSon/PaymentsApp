c645181bb2e38cc9bf5c5ad0d16ea1b879f4ba9e
neliSun <nelson.chow91.nc@gmail.com>
2017-03-14 21:04:55 -0700

    Initial Commit

---
+++ PaymentsApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PaymentsApp
+//
+//  Created by Nelson Chow on 2017-03-14.
+//  Copyright © 2017 Nelson Chow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
